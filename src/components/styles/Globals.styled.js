6686f524897751a05c2db2093e737a60d28e57c2
Joshua Lorenzo <joshualorenzo.dev@gmail.com>
2022-02-22 20:00:56 -0800

    Implements Globalstyles

---
+++ src/components/styles/Globals.styled.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*,
+*::before,
+*::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html {
+    background: ${({ theme }) => theme.colors.navyBlue};
+    scroll-behavior: smooth;
+    color: ${({ theme }) => theme.colors.white};
+    font-family: ${({ theme }) => theme.fonts.primaryFont};
+}
+
+a {
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.green};
+    transition: all 250ms ease-in-out;
+
+    &:hover {
+        color: ${({ theme }) => theme.colors.orange};
+    };
+}
+
+ul {
+    list-style-type: none;
+}
+
+hr {
+    background: linear-gradient(
+        to right,
+        ${({ theme }) => theme.colors.orange} 0%,
+        ${({ theme }) => theme.colors.purple} 100%     
+    );
+    
+    border: 0;
+    height: 1px;
+}
+`;
---
+++ src/theme/default.js
@@ -0,0 +1,20 @@
+export const Theme = {
+  fonts: {
+    primaryFont: "Share Tech Mono, monospace",
+    secondaryFont: "Poppins, sans-serif",
+  },
+  colors: {
+    white: "rgba(240, 247, 255, 1)",
+    navyBlue: "rgba(2, 12, 27, 1)",
+    lightNavyBlue: "rgba(4, 22, 48, 1)",
+    green: "rgba(0, 206, 158, 1)",
+    orange: "rgba(249, 105, 14, 1)",
+    purple: "rgba(102, 51, 153, 1)",
+  },
+  breakpoints: {
+    sm: "screen and (max-width: 640px)",
+    md: "screen and (max-width: 768px)",
+    lg: "screen and (max-width: 1024px)",
+    xl: "screen and (max-width: 1280px)",
+  },
+};
